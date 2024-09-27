bccf9463b0ec5bde1019f9b7542ceccf6146df3a
lf14 <54648106+lf14@users.noreply.github.com>
2024-09-27 22:49:25 +0000

    [Completado] -app.js funciones de conversión de moneda -test.js test de funciones

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-// We declare the function with the exact name "fromEuroToDollar"
-const fromEuroToDollar = function(valueInEuro) {
-    // Convert the given valueInEuro to dollars
+
+const fromEuroToDollar = function (valueInEuro) {
+
     let valueInDollar = valueInEuro * 1.07;
-    // return the dollar value
+
     return valueInDollar;
 }
 
-const fromDollarToYen = function(valueInDollar) {
+const fromDollarToYen = function (valueInDollar) {
     let valueInYen = valueInDollar * 156.5;
     return valueInYen;
 }
 
-const fromYenToPound = function (valueInYen){
+const fromYenToPound = function (valueInYen) {
     let valueInPound = valueInYen * 0.87;
     return valueInPound;
 }
-// This is my function that sums two numbers
-const sum = (a,b) => {
+const sum = (a, b) => {
     return a + b
 }
 
-// Just a console log for ourselves
-console.log(sum(7,3))
 
-// Export the function to be used on other files 
-// (similar to the keyword "export" when using webpack)
-// We include fromEuroToDollar here as well because it needs to be exported
-module.exports = { sum, fromEuroToDollar,fromDollarToYen, fromYenToPound }
+console.log(sum(7, 3))
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
