e234d3e3393731039d78dc1a19aacd008fd8404d
lf14 <54648106+lf14@users.noreply.github.com>
2024-09-27 22:31:53 +0000

    test.js- se agregan los test

--- test.js
+++ test.js
@@ -22,12 +22,12 @@ test("One euro should be 1.07 dollars", function() {
     // This is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
-test("One dollar should be 156.5 JPY)", function() {
+test("Ten dollars should be 1565 JPY)", function() {
     const { fromDollarToYen } = require('./app.js')
     expect(fromDollarToYen(10)).toBe(1565);
 })
 
-test("One Yen should be 0.87 GBP", function() {
+test("Ten Yens should be 8.7 GBP", function() {
     const { fromYenToPound } = require('./app.js')
     expect(fromYenToPound(10)).toBe(8.7);
 })
