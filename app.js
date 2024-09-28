19eca164ba73b4dda55adc0f10b221c87448b810
lf14 <54648106+lf14@users.noreply.github.com>
2024-09-28 00:17:22 +0000

    [Checking]
    app.js-
    test.js-

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// We declare the function with the exact name "fromEuroToDollar"
 
-const fromEuroToDollar = function (valueInEuro) {
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
 
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
     let valueInDollar = valueInEuro * 1.07;
-
+    // return the dollar value
     return valueInDollar;
 }
 
--- test.js
+++ test.js
@@ -8,18 +8,18 @@ test('adds 14 + 9 to equal 23', () => {
 
     expect(total).toBe(23);
 });
-test("One euro should be 1.07 dollars", function () {
-
+test("One euro should be 1.07 dollars", function() {
+    // Import the function from app.js
     const { fromEuroToDollar } = require('./app.js');
 
-
+    // Use the function like its supposed to be used
     const dollars = fromEuroToDollar(3.5);
 
-
-    const expected = 3.5 * 1.07;
-
-
-    expect(fromEuroToDollar(3.5)).toBe(3.745);
+    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
+    const expected = 3.5 * 1.07; 
+    
+    // This is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
 test("Ten dollars should be 1565 JPY)", function () {
     const { fromDollarToYen } = require('./app.js')
